b100d3afd08c5913f5f02260cccede3d5a28d068
naveen <naveennanigande422@gmail.com>
2025-02-22 21:08:19 +0530

    exploring GET, Delete, update and patch

--- src/app.js
+++ src/app.js
@@ -3,17 +3,31 @@ const express = require("express");
 let app = new express(); // creatsing Instance
 
 
-// app.get("/",(req, res)=> {
-//   res.send('hello naveen')
-// });
+// all methods will call here like GET , POST, DELete, update
+app.use('/user',(req,res) => {
+    res.send('using use route')
+})
 
-app.get("/firstPage",(req, res)=> {
-    res.send('First page')
-  })
+app.get('/user',(req,res)=>{
+    res.send({'name':'naveen Gande', 'age':25})
+})
 
-  app.get("/secoundPage",(req, res)=> {
-    res.send('secound Page')
-  })
+app.post('/user',(req,res)=>{
+    res.send('data saved and updated in database')
+})
+
+app.delete('/user',(req,res)=>{
+    res.send('data delated in data base')
+})
+
+app.patch('/user',(req,res)=>{
+    res.send('data updated partially in database')
+})
+
+  app.use("/",(req, res)=> {
+    res.send('hello naveen')
+  });
+  
 
 app.listen('3000', ()=>{
     console.log('server is listening to port number 3000')
