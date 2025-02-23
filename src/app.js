463db85f07cc6d38166a4e547d65b8ec638dede6
naveen <naveennanigande422@gmail.com>
2025-02-23 13:02:03 +0530

    routing nodejs

--- src/app.js
+++ src/app.js
@@ -4,29 +4,30 @@ let app = new express(); // creatsing Instance
 
 
 // all methods will call here like GET , POST, DELete, update
-app.use('/user',(req,res) => {
-    res.send('using use route')
-})
+// app.use('/user',(req,res) => {
+//     res.send('using user route')
+// })
 
-app.get('/user',(req,res)=>{
+app.get('/user/:userId/:name/:password', (req,res)=>{
+    console.log(req.params)
     res.send({'name':'naveen Gande', 'age':25})
 })
 
-app.post('/user',(req,res)=>{
-    res.send('data saved and updated in database')
-})
+// app.post('/user',(req,res)=>{
+//     res.send('data saved and updated in database')
+// })
 
-app.delete('/user',(req,res)=>{
-    res.send('data delated in data base')
-})
+// app.delete('/user',(req,res)=>{
+//     res.send('data delated in data base')
+// })
 
-app.patch('/user',(req,res)=>{
-    res.send('data updated partially in database')
-})
+// app.patch('/user',(req,res)=>{
+//     res.send('data updated partially in database')
+// })
 
-  app.use("/",(req, res)=> {
-    res.send('hello naveen')
-  });
+//   app.use("/",(req, res)=> {
+//     res.send('hello naveen')
+//   });
   
 
 app.listen('3000', ()=>{
