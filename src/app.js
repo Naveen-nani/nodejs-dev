54eeb77bb5bafe3029e33def9ca2ff2824cfefab
naveen <naveennanigande422@gmail.com>
2025-03-04 08:35:45 +0530

    handiling route handilers

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ const express = require("express");
 
 let app = new express(); // creatsing Instance
 
+// syntax for route handiler
+// app.get('',[(rh1),(rh2),(rh3)]) we can keep all route handilters in array
+
 
 app.get('/user',[(req,res,next) => {
     console.log('first Route handiler');
     next();
-    // res.send('I am first route handiler');
+    res.send('I am first route handiler');
 },(req,res,next) => {
     console.log('2 nd route handiler');
-    // res.send('I am 2nd  route handiler')
+    res.send('I am 2nd  route handiler')
     next();
 },(req,res,next) => {
     console.log('3 nd route handiler');
