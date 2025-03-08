2ed0ed50af785228007c74f4235701e9bbbd0d89
naveen <naveennanigande422@gmail.com>
2025-03-08 14:48:02 +0530

    error handiling

--- src/app.js
+++ src/app.js
@@ -1,40 +1,28 @@
 const express = require("express");
 
-let app = new express(); // creatsing Instance
+let app = new express(); // creating Instance
 
-let {adminAuth, userAuth} = require('./middleware/auth')
+let { adminAuth, userAuth } = require('./middleware/auth')
 
-//handiling route handilers Indivisually.
+app.get("/admin1", (req, res) => {
 
-//the function which send responce back is called request handiler
-// remaing are middlewares
 
-//middileware authorization  for Admin and get data
+    try {
+        throw new Error('anckokrmk');
+        res.send('hello admin');
+    } catch (err) {
+        res.status(500).send('please contaict systeam adminter');
+    }
+}
+)
 
-app.use('/admin',adminAuth)
-
-app.get('/admin/getAllData',(req,res)=> {
-    res.send('Admin authorized successfully and get data')
+// middile ware that handile error we need to keep this line end of the all functions
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(500).send('please contact sysyteam admin');
+    }
 })
 
-//middileware authorization  for user to get data 
-app.get('/user/data', userAuth,(req,res)=> {
-    res.send('user authorized and get data');
-});
-
-
-//middileware authorization not implemented  for user to login
-app.get('/user/login',(req,res)=> {
-    res.send('user loggined');
-})
-
-
-
-
-
-
-  
-
-app.listen('3000', ()=>{
+app.listen('3000', () => {
     console.log('server is listening to port number 3000')
 })
