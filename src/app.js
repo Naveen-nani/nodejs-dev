3c32f0842c48d4c1d838008d51f80d7a4c12f41d
naveen <naveennanigande422@gmail.com>
2025-03-10 08:08:35 +0530

    post API changes and add Data into compass

--- src/app.js
+++ src/app.js
@@ -1,16 +1,37 @@
 const express = require("express");
 const connectDB = require('./config/database');
+const User = require('./models/user')
 
-let app = new express(); // creating Instance
+const app = new express(); // creating Instance
 
 
+app.post('/signup', async (req,res)=> {
+  
+    const userObj = {
+            firstName:'test1',
+            lastName:'John1',
+            email:'john1@gmail.com',
+            password: 'john1@123'
+        }
+
+        const user = new User(userObj)
+
+// to handile errors we need to keep our code into try and catch block && all of the (most of the) mongoose functions are promises so we nned to use async N await.
+    try{
+        await user.save();
+        res.send('user created scuccessfully in to database');
+    } catch(err) {
+        res.status(500).send('data not saved into server', err.message)
+    }
+    
+})
 // here we need to coonect DB first then we need to start the server.
 connectDB().then(()=> {
     console.log('Database connect is scussess');
-    app.listen('3000', () => {
-        console.log('server is listening to port number 3000')
+    app.listen('7777', () => {
+        console.log('server is listening to port number 7777')
     })
 }).catch((err) => {
-    console.error('Databse cant be established');
+    console.error('Databse cant be established', err.message);
 })
 
--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 
 const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://naveennanigande422:4aFIe16KLvgqtU55@naveennode.hubw1.mongodb.net/');
+    await mongoose.connect('mongodb+srv://naveennanigande422:4aFIe16KLvgqtU55@naveennode.hubw1.mongodb.net/devTenderApp');
 }
 
 module.exports = connectDB;
