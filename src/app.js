c1b0e59f2cab0a0ba521276d370d8f11f5d0c57c
naveen <naveennanigande422@gmail.com>
2025-03-13 07:17:56 +0530

    post call with dynamic data and implemented express.json

--- src/app.js
+++ src/app.js
@@ -4,29 +4,28 @@ const User = require('./models/user')
 
 const app = new express(); // creating Instance
 
+//middileware to covernvert JSON to js object
+app.use(express.json());
 
-app.post('/signup', async (req,res)=> {
-  
-    const userObj = {
-            firstName:'test1',
-            lastName:'John1',
-            email:'john1@gmail.com',
-            password: 'john1@123'
-        }
+app.post('/signup', async (req, res) => {
+    // signup with Dynamic data (postman).
+    // creating new Instance of the model
+    const user = new User(req.body);
 
-        const user = new User(userObj)
 
-// to handile errors we need to keep our code into try and catch block && all of the (most of the) mongoose functions are promises so we nned to use async N await.
-    try{
+    // to handile errors we need to keep our code into try and catch block && all of the (most of the) mongoose functions are promises so we nned to use async N await.
+    try {
         await user.save();
         res.send('user created scuccessfully in to database');
-    } catch(err) {
-        res.status(500).send('data not saved into server', err.message)
+    } catch (err) {
+        // res.status(500).send('data not saved into server', err.message);
+        res.send(500, 'data not saved into server', err.message)
+        // res.status(500).send(err.message);
     }
-    
+
 })
 // here we need to coonect DB first then we need to start the server.
-connectDB().then(()=> {
+connectDB().then(() => {
     console.log('Database connect is scussess');
     app.listen('7777', () => {
         console.log('server is listening to port number 7777')
