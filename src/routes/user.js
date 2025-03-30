da3375f44167610460da2487900d0b4aa1cfdd43
naveen <naveennanigande422@gmail.com>
2025-03-30 19:13:40 +0530

    feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middleware/auth");
-const ConnectionRequest = require("../models/sendConnect")
+const ConnectionRequest = require("../models/sendConnect");
+const  User = require("../models/user")
 
 // Get all  pending connection Request from loggedIn User.
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -12,8 +13,8 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
         const pendeningConnectionRequest = await ConnectionRequest.find({
             toUserId: logedInUser._id,
             status: 'intrested'
-        // }).populate("fromUserId", "firstName, lastName, age, photoUrl, about, skills");
-        }).populate("fromUserId", ["firstName", "lastName", "age", "photoUrl", "about", "skills"]);
+        }).populate("fromUserId", "firstName lastName age photoUrl about skills");
+        // }).populate("fromUserId", ["firstName", "lastName", "age", "photoUrl", "about", "skills"]);
 
 
         res.json({ message: `Data fetched succesfully`, data: pendeningConnectionRequest })
@@ -49,6 +50,49 @@ userRouter.get('/user/connections', userAuth, async (req,res) => {
 
     } catch (err) {
         res.status(400).send('Error :', err.message)
+    }
+})
+
+
+userRouter.get("/feeds", userAuth,  async (req,res) => {
+   
+
+    try {
+
+        const loggedInUser = req.user;
+
+        //find all the conncetion request sended by user or recived by user.
+        const connectionSentOrRecevied = await ConnectionRequest.find({
+            $or : [
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId");
+
+       // hiding connection send or recived users
+
+       const hideUsers = new Set();
+       connectionSentOrRecevied.forEach((connect) => {
+        hideUsers.add(connect.fromUserId.toString());
+        hideUsers.add(connect.toUserId.toString());
+       })
+
+
+       //removing users and logged in user. same skilled users need to match.
+        const user = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsers) } },
+                { _id:  { $ne: loggedInUser._id }},
+                { skills: {$in: loggedInUser.skills}}
+            ]
+        })
+
+       console.log(user)
+
+        res.json(user)
+
+    } catch (err) {
+        res.json({messagee : err.message})
     }
 })
 
